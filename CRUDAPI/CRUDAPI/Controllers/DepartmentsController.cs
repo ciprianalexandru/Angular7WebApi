579a8af1bc6cf16de7b082aa9a81a6aff741f5d0
Alexandru Ciprian <ciprian_alexandru@hotmail.com>
2020-10-29 17:15:57 +0200

    Add to repository

---
+++ CRUDAPI/CRUDAPI/Controllers/DepartmentsController.cs
@@ -0,0 +1,129 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CRUDAPI.Models;
+
+namespace CRUDAPI.Controllers
+{
+    public class DepartmentsController : ApiController
+    {
+        private CipDbEntities db = new CipDbEntities();
+
+        // GET: api/Departments
+        [HttpGet]
+        [Route("api/Departments/GetDepartments")]
+        public IQueryable<Department> GetDepartments()
+        {
+            return db.Departments;
+        }
+
+        // GET: api/Departments/5
+        //[ResponseType(typeof(Department))]
+        [HttpGet]
+        [Route("api/Departments/GetDepartmentById/{id}")]
+        public IHttpActionResult GetDepartment(int id)
+        {
+            Department department = db.Departments.Find(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(department);
+        }
+
+        // PUT: api/Departments/5
+        //[ResponseType(typeof(void))]
+        [HttpPut]
+        [Route("api/Departments/UpdateDepartment")]
+        public IHttpActionResult PutDepartment(Department department)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+           
+            db.Entry(department).State = EntityState.Modified;
+
+            try
+            {
+                Department objDepartment = new Department();
+                objDepartment = db.Departments.Find(department.Id);
+                if (objDepartment != null)
+                {
+                    objDepartment.Name = department.Name;
+                    objDepartment.Description = department.Description;                   
+                }
+
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DepartmentExists(department.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Departments        
+        [HttpPost]
+        [Route("api/Departments/InsertDepartment")]
+        //[ResponseType(typeof(Department))]
+        public IHttpActionResult PostDepartment(Department department)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Departments.Add(department);
+            db.SaveChanges();
+
+            //return CreatedAtRoute("DefaultApi", new { id = department.Id }, department);
+
+            return Ok(department);
+        }
+
+        // DELETE: api/Departments/5
+        [HttpPost]
+        [Route("api/Departments/DeleteDepartment/{id}")]
+        //[ResponseType(typeof(Department))]
+        public IHttpActionResult DeleteDepartment(int id)
+        {
+            Department department = db.Departments.Find(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            db.Departments.Remove(department);
+            db.SaveChanges();
+
+            return Ok(department);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DepartmentExists(int id)
+        {
+            return db.Departments.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ CRUDAPI/CRUDAPI/Controllers/EmployeeDetailsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CRUDAPI.Models;
+
+namespace CRUDAPI.Controllers
+{
+    public class EmployeeDetailsController : ApiController
+    {
+        private CipDbEntities db = new CipDbEntities();
+
+        // GET: api/EmployeeDetails
+        [HttpGet]
+        [Route("api/EmployeeDetails/AllEmployeeDetails")]
+        public IQueryable<EmployeeDetail> GetEmployeeDetails()
+        {
+            return db.EmployeeDetails;
+        }
+
+        // GET: api/EmployeeDetails/5        
+        //[ResponseType(typeof(EmployeeDetail))]
+        [HttpGet]
+        [Route("api/EmployeeDetails/GetEmployeeDetailsById/{id}")]
+        public IHttpActionResult GetEmployeeDetail(int id)
+        {
+            EmployeeDetail employeeDetail = db.EmployeeDetails.Find(id);
+            if (employeeDetail == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employeeDetail);
+        }
+
+        // PUT: api/EmployeeDetails/5
+        //[ResponseType(typeof(void))]
+        [HttpPut]
+        [Route("api/EmployeeDetails/UpdateEmployeeDetails")]
+        public IHttpActionResult PutEmployeeDetail(EmployeeDetail employeeDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Entry(employeeDetail).State = EntityState.Modified;
+           
+            try
+            {
+                EmployeeDetail objEmp = new EmployeeDetail();
+                objEmp = db.EmployeeDetails.Find(employeeDetail.EmpId);
+                if (objEmp != null)
+                {
+                    objEmp.EmpName = employeeDetail.EmpName;
+                    objEmp.Address = employeeDetail.Address;
+                    objEmp.EmailId = employeeDetail.EmailId;
+                    objEmp.DateOfBirth = employeeDetail.DateOfBirth;
+                    objEmp.Gender = employeeDetail.Gender;
+                    objEmp.PinCode = employeeDetail.PinCode;
+                }
+
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeDetailExists(employeeDetail.EmpId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/EmployeeDetails
+        [HttpPost]
+        [Route("api/EmployeeDetails/InsertEmployeeDetails")]
+        //[ResponseType(typeof(EmployeeDetail))]
+        public IHttpActionResult PostEmployeeDetail(EmployeeDetail employeeDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.EmployeeDetails.Add(employeeDetail);
+            db.SaveChanges();
+
+            //return CreatedAtRoute("DefaultApi", new { id = employeeDetail.EmpId }, employeeDetail);
+            return Ok(employeeDetail);
+        }
+
+        // DELETE: api/EmployeeDetails/5
+        [ResponseType(typeof(EmployeeDetail))]
+        public IHttpActionResult DeleteEmployeeDetail(int id)
+        {
+            EmployeeDetail employeeDetail = db.EmployeeDetails.Find(id);
+            if (employeeDetail == null)
+            {
+                return NotFound();
+            }
+
+            db.EmployeeDetails.Remove(employeeDetail);
+            db.SaveChanges();
+
+            return Ok(employeeDetail);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmployeeDetailExists(int id)
+        {
+            return db.EmployeeDetails.Count(e => e.EmpId == id) > 0;
+        }
+    }
+}
